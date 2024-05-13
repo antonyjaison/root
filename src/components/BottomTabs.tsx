42369f954aa1bac367af561dd1eecaf281f5c14b
Antony Jaison <antonyjaison639@gmail.com>
2024-05-13 18:39:31 +0530

    added weather info and resources

--- src/components/BottomTabs.tsx
+++ src/components/BottomTabs.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const BottomTabs = () => {
     return (
         <div className=' w-full h-24 bg-white shadow absolute bottom-0 left-0'>
-            
+
         </div>
     )
 }
