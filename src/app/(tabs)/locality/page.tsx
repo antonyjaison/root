13277e4855919b8468eef6e205f3e104483f7bb6
Antony Jaison <antonyjaison639@gmail.com>
2024-05-13 23:03:05 +0530

    added suggestions tab

--- src/app/(tabs)/locality/page.tsx
+++ src/app/(tabs)/locality/page.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { ChevronRight } from 'lucide-react'
+import ResourceCard from '@/components/ResourceCard'
 
 
 const LocalityPage = () => {
@@ -11,8 +13,47 @@ const LocalityPage = () => {
                     <TabsTrigger className=' w-[100%]' value="overview">Overview</TabsTrigger>
                     <TabsTrigger className=' w-[100%]' value="suggestions">Suggestions</TabsTrigger>
                 </TabsList>
-                <TabsContent value="overview">Make changes to your account here.</TabsContent>
-                <TabsContent value="suggestions">Change your password here.</TabsContent>
+                <TabsContent value="overview">
+
+                    <div className=' flex flex-col gap-5 mt-5'>
+                        <div className=' w-full rounded-3xl px-6 py-7 bg-white shadow-md'>
+                            <div className=' h-fit flex justify-between items-center w-full'>
+                                <h4 className=' text-lg'>NDVI Score</h4>
+                                <ChevronRight size={20} />
+                            </div>
+
+                            <div>
+                                {/* content */}
+                            </div>
+
+                        </div>
+
+                        <div>
+                            <h4 className=' text-lg'>Basic Characteristics</h4>
+                            <div className=' grid grid-cols-3 gap-5 mt-3'>
+                                <ResourceCard bg_color='#fff' />
+                                <ResourceCard bg_color='#fff' />
+                                <ResourceCard bg_color='#fff' />
+                                <ResourceCard bg_color='#fff' />
+                                <ResourceCard bg_color='#fff' />
+                                <ResourceCard bg_color='#fff' />
+                            </div>
+                        </div>
+
+
+
+                    </div>
+
+                </TabsContent>
+                <TabsContent value="suggestions">
+                    <div className=' w-full rounded-3xl px-6 py-7 bg-white shadow-md mt-5'>
+                        <p>Field F1 (Loam Soil):</p>
+                        <ul className=' list-disc ml-6'>
+                            <li>Vegetables: Loam soil is excellent for various vegetables like tomatoes, peppers, beans, peas, squash, and leafy greens. The moderate temperature and moisture levels in the data suggest a warm season for growing.</li>
+                            <li>Fruits: Melons, watermelons, and some berry varieties can also thrive in loam soil with proper irrigation and sunlight.</li>
+                        </ul>
+                    </div>
+                </TabsContent>
             </Tabs>
 
 
